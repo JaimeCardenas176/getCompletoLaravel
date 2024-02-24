14ebae2e23c970a366a6f9a4d5db9536b47313a9
Jaime Cárdenas <jaime.cardenas176@gmail.com>
2024-02-24 21:37:23 +0100

    haciendo una peticion get que me devuelve usuarios de una base de datos

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+
+    public function findAll(){
+
+        print("llega\n");
+        $users = User::all();
+
+        return $users;
+    }
+}
